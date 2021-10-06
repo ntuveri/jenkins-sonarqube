c315efc6cbf74afd7b254f33ad8aee8fb9e017d6
Nicola Tuveri <ntuveri@gmail.com>
2021-10-06 12:32:53 +0200

    initial version

---
+++ Jenkinsfile
@@ -0,0 +1,13 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Hello world') {
+            steps {
+                script {
+                    echo "Build based on Jenkinsfile"
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/com/example/JenkinsSonarQube/JenkinsSonarQubeApplication.java
@@ -0,0 +1,13 @@
+package com.example.JenkinsSonarQube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSonarQubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsSonarQubeApplication.class, args);
+	}
+
+}
